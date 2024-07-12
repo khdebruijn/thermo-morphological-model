e753257c1513d2aa18b6af9da8f3f4e623ff3fa6
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-07-12 16:25:50 +0200

    setup of new runs

--- runs/runs.bat
+++ runs/runs.bat
@@ -1,16 +1,23 @@
 :: to execute this batch file, cd to project directory and type "runs\runs.bat"
 
-python main.py val3
 python main.py val3_xb
-
-python main_results.py val3 bluff_and_toe
-python main_results.py val3 bed
-python main_results.py val3 heat
-python main_results.py val3 temp_heat
-python main_results.py val3 temp
+python main.py val4
+python main.py val4_xb
 
 python main_results.py val3_xb bluff_and_toe
 python main_results.py val3_xb bed
 python main_results.py val3_xb heat
 python main_results.py val3_xb temp_heat
 python main_results.py val3_xb temp
+
+python main_results.py val4 bluff_and_toe
+python main_results.py val4 bed
+python main_results.py val4 heat
+python main_results.py val4 temp_heat
+python main_results.py val4 temp
+
+python main_results.py val4_xb bluff_and_toe
+python main_results.py val4_xb bed
+python main_results.py val4_xb heat
+python main_results.py val4_xb temp_heat
+python main_results.py val4_xb temp
