eae003b153d82df950fa749e44dafb68c5b15411
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-07-12 11:07:50 +0200

    Update runs.bat

--- runs/runs.bat
+++ runs/runs.bat
@@ -3,5 +3,14 @@
 python main.py val3
 python main.py val3_xb
 
-python main_results.py val3 bluff_and_toe bed heat temp_heat temp
-python main_results.py val3_xb bluff_and_toe bed heat temp_heat temp
+python main_results.py val3 bluff_and_toe
+python main_results.py val3 bed
+python main_results.py val3 heat
+python main_results.py val3 temp_heat
+python main_results.py val3 temp
+
+python main_results.py val3_xb bluff_and_toe
+python main_results.py val3_xb bed
+python main_results.py val3_xb heat
+python main_results.py val3_xb temp_heat
+python main_results.py val3_xb temp
