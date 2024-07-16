7aa550a04dc396a60715e5162e386805b0f3d3d2
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-07-16 15:31:07 +0200

    setup new sims and create some figures

--- runs/runs.bat
+++ runs/runs.bat
@@ -1,23 +1,7 @@
 :: to execute this batch file, cd to project directory and type "runs\runs.bat"
 
-python main.py val3_xb
-python main.py val4
-python main.py val4_xb
+python main.py val5_xb
+python main.py val5_xb_ne_off
 
-python main_results.py val3_xb bluff_and_toe
-python main_results.py val3_xb bed
-python main_results.py val3_xb heat
-python main_results.py val3_xb temp_heat
-python main_results.py val3_xb temp
-
-python main_results.py val4 bluff_and_toe
-python main_results.py val4 bed
-python main_results.py val4 heat
-python main_results.py val4 temp_heat
-python main_results.py val4 temp
-
-python main_results.py val4_xb bluff_and_toe
-python main_results.py val4_xb bed
-python main_results.py val4_xb heat
-python main_results.py val4_xb temp_heat
-python main_results.py val4_xb temp
+python main_results.py val5_xb all
+python main_results.py val5_xb_ne_off all
