50cb415c0ff8efc8b5277d63b10c4719ce396011
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-09-12 17:17:11 +0200

    Update runs.bat

--- runs/runs.bat
+++ runs/runs.bat
@@ -1,7 +1,4 @@
 :: to execute this batch file, cd to project directory and type "runs\runs.bat"
 
-python main.py val5_xb
-python main.py val5_xb_ne_off
-
-python main_results.py val5_xb all
-python main_results.py val5_xb_ne_off all
+python main.py val8_xb
+python main_results.py val8_xb all
