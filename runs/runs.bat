ea36d1b378b5de1f4fb3aead619d7dce765066cf
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-09-19 15:29:23 +0200

    setup val9_xb

--- runs/runs.bat
+++ runs/runs.bat
@@ -1,4 +1,4 @@
 :: to execute this batch file, cd to project directory and type "runs\runs.bat"
 
-python main.py val8_xb
-python main_results.py val8_xb all
+python main.py val9_xb
+python main_results.py val9_xb all
