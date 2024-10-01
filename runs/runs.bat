ab829520bcabc64af68332b20c20199618958749
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-10-01 09:13:13 -0700

    setup val_gt3 and val_gt4

--- runs/runs.bat
+++ runs/runs.bat
@@ -1,4 +1,6 @@
 :: to execute this batch file, cd to project directory and type "runs\runs.bat"
 
-python main.py val9_xb
-python main_results.py val9_xb all
+python main.py val_gt3
+python main.py val_gt4
+
+:: python main_results.py val9_xb all
