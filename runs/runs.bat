59e2a0125721f2ed58d13ba6591af8e4cd94ab81
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-10-01 12:31:40 -0700

    setup new runs and bug fix old runs

--- runs/runs.bat
+++ runs/runs.bat
@@ -1,6 +1,6 @@
 :: to execute this batch file, cd to project directory and type "runs\runs.bat"
 
-python main.py val_gt5
-python main.py val_gt6
+python main.py val_gt7
+python main.py val_gt8
 
 :: python main_results.py val9_xb all
