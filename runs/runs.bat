03717fba950962f3159d84e5bd120b294875abdf
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-10-15 15:26:09 -0700

    setup show run for tomorrow

--- runs/runs.bat
+++ runs/runs.bat
@@ -1,6 +1,6 @@
 :: to execute this batch file, cd to project directory and type "runs\runs.bat"
 
-python main.py val_gt7
-python main.py val_gt8
+python main.py show_1
+python main_results.py show_1 all
 
 :: python main_results.py val9_xb all
