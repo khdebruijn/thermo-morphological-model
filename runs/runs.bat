1778555d02c727cf8550bcca3d757192c6e30866
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-10-16 17:42:59 -0700

    Update runs.bat

--- runs/runs.bat
+++ runs/runs.bat
@@ -1,6 +1,14 @@
 :: to execute this batch file, cd to project directory and type "runs\runs.bat"
 
-python main.py show_1
-python main_results.py show_1 all
+:: python main.py show_1
+:: python main_results.py show_1 all
+
+python main.py val13_xb
+python main.py val_gt16
+python main.py val_gt17
+python main.py val_gt18
+
+python main_results.py val13_xb all
+
 
 :: python main_results.py val9_xb all
