68763fda79473d81470afd7e17818958d11ef7aa
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-10-30 14:57:24 -0700

    setup new runs

--- runs/runs1.bat
+++ runs/runs1.bat
@@ -1,4 +1,4 @@
 :: to execute this batch file, cd to project directory and type "runs\runs.bat"
 
-python main.py val_per1_1
-python main_results.py val_per1_1 all 1300 1400
+python main.py val_per1_2
+python main_results.py val_per1_2 all 1300 1400
--- runs/runs3.bat
+++ runs/runs3.bat
@@ -1,4 +1,4 @@
 :: to execute this batch file, cd to project directory and type "runs\runs.bat"
 
-python main.py val_per3_1
-python main_results.py val_per3_1 all 1300 1400
+python main.py val_per3_2
+python main_results.py val_per3_2 all 1300 1400
