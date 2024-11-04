427d8e6046363b2c255b2e89db1ed7b124ba2e37
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-11-04 13:36:19 -0800

    Update collect_erosion.py

--- results/collect_erosion.py
+++ results/collect_erosion.py
@@ -17,8 +17,7 @@ runid = sys.argv[1]
 
 run_output_dir = Path(f"p:/11210070-usgscoop-202324-arcticxb/runs/{runid}/")
 
-
-fnames = os.listdir(run_output_dir)
+output_ids = np.int32(np.loadtxt(os.path.join(run_output_dir, "timetsep_output_ids.txt")))
 
 t_list = []
 x_shore_line_list = []
@@ -26,26 +25,27 @@ x_bluff_edge_list = []
 
 i = 0
 
-for fname in fnames:
-
-    if fname[0] == '0' and '.nc' in fname:
-        
-        ds = xr.load_dataset(os.path.join(run_output_dir, fname))
-        
-        t = ds.timestamp
-        
-        xgr = ds.xgr.values
-        zgr = ds.zgr.values
-        
-        x_shore_line = calculate_shoreline_position(xgr, zgr)
-        x_bluff_edge, __, __ = calculate_bluff_edge_toe_position(xgr, zgr)
-        
-        t_list.append(t)
-        x_shore_line_list.append(x_shore_line)
-        x_bluff_edge_list.append(x_bluff_edge)
+for output_id in output_ids:
+    
+    fname = (10 - len(str(int(output_id)))) * '0' + str(int(output_id)) + ".nc"
+            
+    ds = xr.load_dataset(os.path.join(run_output_dir, fname))
+    
+    t = ds.timestamp
+    
+    xgr = ds.xgr.values
+    zgr = ds.zgr.values
+    
+    x_shore_line = calculate_shoreline_position(xgr, zgr)
+    x_bluff_edge, __, __ = calculate_bluff_edge_toe_position(xgr, zgr)
+    
+    t_list.append(t)
+    x_shore_line_list.append(x_shore_line)
+    x_bluff_edge_list.append(x_bluff_edge)
 
     i += 1
-    print(f'{i}/{len(fnames)}')
+    
+    print(f'{i}/{len(output_ids)}')
 
 df = pd.DataFrame(data={
     'time':t_list, 
