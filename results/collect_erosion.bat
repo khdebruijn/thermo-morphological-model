f371fb400e90c2719ece6886c56c150244e825e8
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-11-04 13:36:24 -0800

    Create collect_erosion.bat

---
+++ results/collect_erosion.bat
@@ -0,0 +1,21 @@
+:: run to generate erosion files for each run
+python results\collect_erosion.py val_per1_1
+python results\collect_erosion.py val_per1_2
+python results\collect_erosion.py val_per1_3
+python results\collect_erosion.py val_per1_4
+python results\collect_erosion.py val_per1_5
+python results\collect_erosion.py val_per1_6
+
+python results\collect_erosion.py val_per2_1
+python results\collect_erosion.py val_per2_2
+python results\collect_erosion.py val_per2_3
+python results\collect_erosion.py val_per2_4
+python results\collect_erosion.py val_per2_5
+python results\collect_erosion.py val_per2_6
+
+python results\collect_erosion.py val_per3_1
+python results\collect_erosion.py val_per3_2
+python results\collect_erosion.py val_per3_3
+python results\collect_erosion.py val_per3_4
+python results\collect_erosion.py val_per3_5
+python results\collect_erosion.py val_per3_6
