ca1fadfe87833dd60f7a376875b0c6048eeb2f69
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-11-11 18:31:13 +0100

    setup results run

--- results/collect_erosion.bat
+++ results/collect_erosion.bat
@@ -5,17 +5,17 @@ python results\collect_erosion.py val_per1_3
 python results\collect_erosion.py val_per1_4
 python results\collect_erosion.py val_per1_5
 python results\collect_erosion.py val_per1_6
+python results\collect_erosion.py val_per1_7
+python results\collect_erosion.py val_per1_8
+python results\collect_erosion.py val_per1_9
+python results\collect_erosion.py val_per1_10
+python results\collect_erosion.py val_per1_11
+python results\collect_erosion.py val_per1_12
+python results\collect_erosion.py val_per1_13
+python results\collect_erosion.py val_per1_14
 
 python results\collect_erosion.py val_per2_1
 python results\collect_erosion.py val_per2_2
-python results\collect_erosion.py val_per2_3
-python results\collect_erosion.py val_per2_4
-python results\collect_erosion.py val_per2_5
-python results\collect_erosion.py val_per2_6
 
 python results\collect_erosion.py val_per3_1
 python results\collect_erosion.py val_per3_2
-python results\collect_erosion.py val_per3_3
-python results\collect_erosion.py val_per3_4
-python results\collect_erosion.py val_per3_5
-python results\collect_erosion.py val_per3_6
