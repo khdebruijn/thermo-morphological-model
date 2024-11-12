da973217366cb24dae256863a678a4a15a25a035
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-11-12 14:25:54 +0100

    collect erosion fix

--- results/collect_erosion.bat
+++ results/collect_erosion.bat
@@ -1,21 +1,2 @@
 :: run to generate erosion files for each run
-python results\collect_erosion.py val_per1_1
 python results\collect_erosion.py val_per1_2
-python results\collect_erosion.py val_per1_3
-python results\collect_erosion.py val_per1_4
-python results\collect_erosion.py val_per1_5
-python results\collect_erosion.py val_per1_6
-python results\collect_erosion.py val_per1_7
-python results\collect_erosion.py val_per1_8
-python results\collect_erosion.py val_per1_9
-python results\collect_erosion.py val_per1_10
-python results\collect_erosion.py val_per1_11
-python results\collect_erosion.py val_per1_12
-python results\collect_erosion.py val_per1_13
-python results\collect_erosion.py val_per1_14
-
-python results\collect_erosion.py val_per2_1
-python results\collect_erosion.py val_per2_2
-
-python results\collect_erosion.py val_per3_1
-python results\collect_erosion.py val_per3_2
--- results/collect_erosion.py
+++ results/collect_erosion.py
@@ -1,3 +1,4 @@
+# import packages
 import os
 from pathlib import Path
 import sys
@@ -12,19 +13,58 @@ sys.path.append(proj_dir)
 
 from utils.bathymetry import calculate_bluff_edge_toe_position, calculate_shoreline_position
 
-
+# read input
 runid = sys.argv[1]
 
 run_output_dir = Path(f"p:/11210070-usgscoop-202324-arcticxb/runs/{runid}/")
 
 output_ids = np.int32(np.loadtxt(os.path.join(run_output_dir, "timestep_output_ids.txt")))
 
+# reference variables
+per1 = {
+    "start_time": pd.to_datetime("2009-08-02"), # these dates are relevant for the bluff edge only
+    "end_time": pd.to_datetime("2011-07-15"),
+    "x_shore_line_start": -1,   # i.e., distance to baseline at start
+    "x_bluff_edge_start": 162.01508156899843,   # i.e., distance to baseline at start
+    "x_shore_line_end": -1,   # i.e., distance to baseline at end
+    "x_bluff_edge_end": 162.4068294126812,   # i.e., distance to baseline at end
+}
+
+per2 = {
+    "start_time": pd.to_datetime("2012-07-11"), # these dates are relevant for the bluff edge only
+    "end_time": pd.to_datetime("2015-07-05"),
+    "x_shore_line_start": -1,   # i.e., distance to baseline at start
+    "x_bluff_edge_start": 164.52084174135774,   # i.e., distance to baseline at start
+    "x_shore_line_end": -1,   # i.e., distance to baseline at end
+    "x_bluff_edge_end": 179.48248124206415,   # i.e., distance to baseline at end
+}
+
+per3 = {
+    "start_time": pd.to_datetime("2016-08-27"), # these dates are relevant for the bluff edge only
+    "end_time": pd.to_datetime("2018-07-30"),
+    "x_shore_line_start": -1,   # i.e., distance to baseline at start
+    "x_bluff_edge_start": 187.25161246050337,   # i.e., distance to baseline at start
+    "x_shore_line_end": -1,   # i.e., distance to baseline at end
+    "x_bluff_edge_end": 195.1759190877606,   # i.e., distance to baseline at end
+}
+
+# get relevant period information
+if 'per1' in runid:
+    per = per1
+elif 'per2' in runid:
+    per = per2
+elif 'per3' in runid:
+    per = per3
+
+# initialize output lists
 t_list = []
 x_shore_line_list = []
 x_bluff_edge_list = []
 
+# used for printing progress to screen
 i = 0
 
+# loop through output
 for output_id in output_ids:
     
     fname = (10 - len(str(int(output_id)))) * '0' + str(int(output_id)) + ".nc"
@@ -54,6 +94,11 @@ df = pd.DataFrame(data={
     'x_shore_line':x_shore_line_list,
     'x_bluff_edge':x_bluff_edge_list,
     })
+
+reference_id = df['time'] == per['start_date']
+reference_offset = df[reference_id].x_bluff_edge.values
+
+df['relative_erosion_bluff_edge'] = df['x_bluff_edge'] - reference_offset + per['x_bluff_edge_start']
 
 save_path = f'./results/erosion_rates/{runid}.csv'
 
