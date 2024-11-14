319cf364318818911a8dfaf73ce194a1cf4dc570
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-11-14 18:06:54 +0100

    setup new runs

--- runs/runs1.bat
+++ runs/runs1.bat
@@ -1,14 +1,8 @@
 :: to execute this batch file, cd to project directory and type "runs\runs.bat"
 :: (did not cancel yet, but can be cancelled)
 
-python main.py val_per1_15
-python main_results.py val_per1_15 all 1300 1400
+python main.py val_per2_15
+python main_results.py val_per2_15 all 1300 1400
 
-python main.py val_per1_16
-python main_results.py val_per1_16 all 1300 1400
-
-python main.py val_per1_17
-python main_results.py val_per1_17 all 1300 1400
-
-python main.py val_per1_18
-python main_results.py val_per1_18 all 1300 1400
+python main.py val_per2_16
+python main_results.py val_per2_16 all 1300 1400
