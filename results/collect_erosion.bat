0b1dfb6d9b3213c7e6ec93617bb60dd8043568ea
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-11-18 13:04:16 +0100

    Update collect_erosion.bat

--- results/collect_erosion.bat
+++ results/collect_erosion.bat
@@ -1,5 +1,5 @@
 :: run to generate erosion files for each run
-@REM python results\collect_erosion.py val_per1_2
+python results\collect_erosion.py val_per1_2
 @REM python results\collect_erosion.py val_per1_3
 @REM python results\collect_erosion.py val_per1_4
 @REM python results\collect_erosion.py val_per1_5
@@ -26,12 +26,14 @@
 @REM python results\collect_erosion.py val_per2_5
 @REM python results\collect_erosion.py val_per2_6
 @REM python results\collect_erosion.py val_per2_7
-python results\collect_erosion.py val_per2_8
+@REM python results\collect_erosion.py val_per2_8
 @REM python results\collect_erosion.py val_per2_9
-python results\collect_erosion.py val_per2_10
+@REM python results\collect_erosion.py val_per2_10
 @REM python results\collect_erosion.py val_per2_11
 @REM python results\collect_erosion.py val_per2_12
 @REM python results\collect_erosion.py val_per2_13
 @REM python results\collect_erosion.py val_per2_14
 @REM python results\collect_erosion.py val_per2_15
 @REM python results\collect_erosion.py val_per2_16
+
+python results\collect_erosion.py val_per3_2
