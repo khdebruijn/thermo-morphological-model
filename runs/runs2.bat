a48976e1e3e59daed84d09bd76e17f329fb5db5d
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-11-19 16:33:06 +0100

    one last set of runs

--- runs/runs2.bat
+++ runs/runs2.bat
@@ -1,5 +1,10 @@
 :: to execute this batch file, cd to project directory and type "runs\runs.bat"
 
-python main.py val_per1_fast
-python main_results.py val_per1_fast all 1300 1400
+python main.py val_per2_17
+python main.py val_per2_18
+python main.py val_per2_19
+
+python main_results.py val_per2_17 all 1300 1400
+python main_results.py val_per2_18 all 1300 1400
+python main_results.py val_per2_19 all 1300 1400
 
