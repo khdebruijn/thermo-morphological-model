65277c3275c4db970ffd3188360db152333cf7cc
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-11-20 18:09:17 +0100

    new runs

--- runs/runs4.bat
+++ runs/runs4.bat
@@ -3,3 +3,6 @@
 @REM python main.py val_per3_fast
 @REM python main_results.py val_per3_fast all 1300 1400
 
+python main.py sa_base
+python main.py sa_lvl3_1
+python main.py sa_lvl3_2
--- runs/runs5.bat
+++ runs/runs5.bat
@@ -1,14 +1,15 @@
 :: to execute this batch file, cd to project directory and type "runs\results.bat"
 
-python main.py val_per2_4
-python main_results.py val_per2_4 all 1300 1400
+@REM python main.py val_per2_4
+@REM python main_results.py val_per2_4 all 1300 1400
 
-python main.py val_per2_6
-python main_results.py val_per2_6 all 1300 1400
+@REM python main.py val_per2_6
+@REM python main_results.py val_per2_6 all 1300 1400
 
-python main.py val_per2_8
-python main_results.py val_per2_8 all 1300 1400
+@REM python main.py val_per2_8
+@REM python main_results.py val_per2_8 all 1300 1400
 
-python main.py val_per2_10
-python main_results.py val_per2_10 all 1300 1400
+@REM python main.py val_per2_10
+@REM python main_results.py val_per2_10 all 1300 1400
 
+python main.py sa_base_alt
