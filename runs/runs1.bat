cd46aee4486dcf1e037be37337c55d20e7b40277
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-11-21 11:10:54 +0100

    re-do best runs

--- runs/runs1.bat
+++ runs/runs1.bat
@@ -3,5 +3,11 @@
 @REM python main.py val_per1_best
 @REM python main_results.py val_per1_best all 1300 1400
 
-python main.py val_per2_17
-python main_results.py val_per2_17 all 1300 1400
+@REM python main.py val_per2_17
+@REM python main_results.py val_per2_17 all 1300 1400
+
+python main.py val_per1_best
+python main.py val_per3_best
+
+python main_results.py val_per1_best all 1300 1400
+python main_results.py val_per3_best all 1300 1400
