46b5ac69d977299124cadaa57fb4db0e2aea1bbc
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-11-21 12:56:02 +0100

    new runs

--- runs/runs3.bat
+++ runs/runs3.bat
@@ -3,5 +3,24 @@
 @REM python main.py val_per3_best
 @REM python main_results.py val_per3_best all 1300 1400
 
-python main.py val_per2_19
-python main_results.py val_per2_19 all 1300 1400
+python main.py sa_lvl1_1
+python main.py sa_lvl1_2
+python main.py sa_lvl1_3
+python main.py sa_lvl1_4
+python main.py sa_lvl1_5
+python main.py sa_lvl1_6
+python main.py sa_lvl1_7
+python main.py sa_lvl1_8
+python main.py sa_lvl1_9
+python main.py sa_lvl1_10
+
+python main.py sa_lvl1_11
+python main.py sa_lvl1_12
+python main.py sa_lvl1_13
+python main.py sa_lvl1_14
+python main.py sa_lvl1_15
+python main.py sa_lvl1_16
+python main.py sa_lvl1_17
+python main.py sa_lvl1_18
+python main.py sa_lvl1_19
+python main.py sa_lvl1_20
--- runs/runs4.bat
+++ runs/runs4.bat
@@ -3,6 +3,24 @@
 @REM python main.py val_per3_fast
 @REM python main_results.py val_per3_fast all 1300 1400
 
-python main.py sa_base
-python main.py sa_lvl3_1
-python main.py sa_lvl3_2
+python main.py sa_lvl1_21
+python main.py sa_lvl1_22
+python main.py sa_lvl1_23
+python main.py sa_lvl1_24
+python main.py sa_lvl1_25
+python main.py sa_lvl1_26
+python main.py sa_lvl1_27
+python main.py sa_lvl1_28
+python main.py sa_lvl1_29
+python main.py sa_lvl1_30
+
+python main.py sa_lvl1_31
+python main.py sa_lvl1_32
+python main.py sa_lvl1_33
+python main.py sa_lvl1_34
+python main.py sa_lvl1_35
+python main.py sa_lvl1_36
+python main.py sa_lvl1_37
+python main.py sa_lvl1_38
+python main.py sa_lvl1_39
+python main.py sa_lvl1_40
