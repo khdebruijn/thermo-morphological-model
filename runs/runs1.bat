ad3c74c518e4019c3e08098d59b08e7d78db11a1
Kevin <75491985+khdebruijn@users.noreply.github.com>
2024-11-29 14:50:46 +0100

    new run

--- runs/runs1.bat
+++ runs/runs1.bat
@@ -6,8 +6,5 @@
 @REM python main.py val_per2_17
 @REM python main_results.py val_per2_17 all 1300 1400
 
-python main.py val_per1_best
-python main.py val_per3_best
-
-python main_results.py val_per1_best all 1300 1400
-python main_results.py val_per3_best all 1300 1400
+python main.py passion_best
+python main_results.py passion_best all 1300 1400
