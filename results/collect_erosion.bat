3a9d6b44526555a599ed7f8e15c495f14ef8888a
Kevin <75491985+khdebruijn@users.noreply.github.com>
2025-01-14 17:33:06 +0100

    Final simulations

--- results/collect_erosion.bat
+++ results/collect_erosion.bat
@@ -54,104 +54,108 @@
 @REM python results\collect_erosion.py val_per3_fast
 
 :: sensitivity runs
-python results\collect_erosion.py sa_base
+@REM python results\collect_erosion.py sa_base
 @REM python results\collect_erosion.py sa_base_alt
 
-python results\collect_erosion.py sa_lvl1_1
-python results\collect_erosion.py sa_lvl1_2
-python results\collect_erosion.py sa_lvl1_3
-python results\collect_erosion.py sa_lvl1_4
-python results\collect_erosion.py sa_lvl1_5
-python results\collect_erosion.py sa_lvl1_6
-python results\collect_erosion.py sa_lvl1_7
-python results\collect_erosion.py sa_lvl1_8
-python results\collect_erosion.py sa_lvl1_9
-python results\collect_erosion.py sa_lvl1_10
-python results\collect_erosion.py sa_lvl1_11
-python results\collect_erosion.py sa_lvl1_12
-python results\collect_erosion.py sa_lvl1_13
-python results\collect_erosion.py sa_lvl1_14
-python results\collect_erosion.py sa_lvl1_15
-python results\collect_erosion.py sa_lvl1_16
-python results\collect_erosion.py sa_lvl1_17
-python results\collect_erosion.py sa_lvl1_18
-python results\collect_erosion.py sa_lvl1_19
-python results\collect_erosion.py sa_lvl1_20
-python results\collect_erosion.py sa_lvl1_21
-python results\collect_erosion.py sa_lvl1_22
-python results\collect_erosion.py sa_lvl1_23
-python results\collect_erosion.py sa_lvl1_24
-python results\collect_erosion.py sa_lvl1_25
-python results\collect_erosion.py sa_lvl1_26
-python results\collect_erosion.py sa_lvl1_27
-python results\collect_erosion.py sa_lvl1_28
-python results\collect_erosion.py sa_lvl1_29
-python results\collect_erosion.py sa_lvl1_30
-python results\collect_erosion.py sa_lvl1_31
-python results\collect_erosion.py sa_lvl1_32
-python results\collect_erosion.py sa_lvl1_33
-python results\collect_erosion.py sa_lvl1_34
-python results\collect_erosion.py sa_lvl1_35
-python results\collect_erosion.py sa_lvl1_36
-python results\collect_erosion.py sa_lvl1_37
-python results\collect_erosion.py sa_lvl1_38
-python results\collect_erosion.py sa_lvl1_39
-python results\collect_erosion.py sa_lvl1_40
-python results\collect_erosion.py sa_lvl1_41
-python results\collect_erosion.py sa_lvl1_42
-python results\collect_erosion.py sa_lvl1_43
-python results\collect_erosion.py sa_lvl1_44
-python results\collect_erosion.py sa_lvl1_45
-python results\collect_erosion.py sa_lvl1_46
-python results\collect_erosion.py sa_lvl1_47
-python results\collect_erosion.py sa_lvl1_48
-python results\collect_erosion.py sa_lvl1_49
-python results\collect_erosion.py sa_lvl1_50
-python results\collect_erosion.py sa_lvl1_51
-python results\collect_erosion.py sa_lvl1_52
-python results\collect_erosion.py sa_lvl1_53
-python results\collect_erosion.py sa_lvl1_54
-python results\collect_erosion.py sa_lvl1_55
-python results\collect_erosion.py sa_lvl1_56
-python results\collect_erosion.py sa_lvl1_57
-python results\collect_erosion.py sa_lvl1_58
-python results\collect_erosion.py sa_lvl1_59
-python results\collect_erosion.py sa_lvl1_60
-python results\collect_erosion.py sa_lvl1_61
-python results\collect_erosion.py sa_lvl1_62
-python results\collect_erosion.py sa_lvl1_63
-python results\collect_erosion.py sa_lvl1_64
-python results\collect_erosion.py sa_lvl1_65
-python results\collect_erosion.py sa_lvl1_66
-python results\collect_erosion.py sa_lvl1_67
-python results\collect_erosion.py sa_lvl1_68
+@REM python results\collect_erosion.py sa_lvl1_1
+@REM python results\collect_erosion.py sa_lvl1_2
+@REM python results\collect_erosion.py sa_lvl1_3
+@REM python results\collect_erosion.py sa_lvl1_4
+@REM python results\collect_erosion.py sa_lvl1_5
+@REM python results\collect_erosion.py sa_lvl1_6
+@REM python results\collect_erosion.py sa_lvl1_7
+@REM python results\collect_erosion.py sa_lvl1_8
+@REM python results\collect_erosion.py sa_lvl1_9
+@REM python results\collect_erosion.py sa_lvl1_10
+@REM python results\collect_erosion.py sa_lvl1_11
+@REM python results\collect_erosion.py sa_lvl1_12
+@REM python results\collect_erosion.py sa_lvl1_13
+@REM python results\collect_erosion.py sa_lvl1_14
+@REM python results\collect_erosion.py sa_lvl1_15
+@REM python results\collect_erosion.py sa_lvl1_16
+@REM python results\collect_erosion.py sa_lvl1_17
+@REM python results\collect_erosion.py sa_lvl1_18
+@REM python results\collect_erosion.py sa_lvl1_19
+@REM python results\collect_erosion.py sa_lvl1_20
+@REM python results\collect_erosion.py sa_lvl1_21
+@REM python results\collect_erosion.py sa_lvl1_22
+@REM python results\collect_erosion.py sa_lvl1_23
+@REM python results\collect_erosion.py sa_lvl1_24
+@REM python results\collect_erosion.py sa_lvl1_25
+@REM python results\collect_erosion.py sa_lvl1_26
+@REM python results\collect_erosion.py sa_lvl1_27
+@REM python results\collect_erosion.py sa_lvl1_28
+@REM python results\collect_erosion.py sa_lvl1_29
+@REM python results\collect_erosion.py sa_lvl1_30
+@REM python results\collect_erosion.py sa_lvl1_31
+@REM python results\collect_erosion.py sa_lvl1_32
+@REM python results\collect_erosion.py sa_lvl1_33
+@REM python results\collect_erosion.py sa_lvl1_34
+@REM python results\collect_erosion.py sa_lvl1_35
+@REM python results\collect_erosion.py sa_lvl1_36
+@REM python results\collect_erosion.py sa_lvl1_37
+@REM python results\collect_erosion.py sa_lvl1_38
+@REM python results\collect_erosion.py sa_lvl1_39
+@REM python results\collect_erosion.py sa_lvl1_40
+@REM python results\collect_erosion.py sa_lvl1_41
+@REM python results\collect_erosion.py sa_lvl1_42
+@REM python results\collect_erosion.py sa_lvl1_43
+@REM python results\collect_erosion.py sa_lvl1_44
+@REM python results\collect_erosion.py sa_lvl1_45
+@REM python results\collect_erosion.py sa_lvl1_46
+@REM python results\collect_erosion.py sa_lvl1_47
+@REM python results\collect_erosion.py sa_lvl1_48
+@REM python results\collect_erosion.py sa_lvl1_49
+@REM python results\collect_erosion.py sa_lvl1_50
+@REM python results\collect_erosion.py sa_lvl1_51
+@REM python results\collect_erosion.py sa_lvl1_52
+@REM python results\collect_erosion.py sa_lvl1_53
+@REM python results\collect_erosion.py sa_lvl1_54
+@REM python results\collect_erosion.py sa_lvl1_55
+@REM python results\collect_erosion.py sa_lvl1_56
+@REM python results\collect_erosion.py sa_lvl1_57
+@REM python results\collect_erosion.py sa_lvl1_58
+@REM python results\collect_erosion.py sa_lvl1_59
+@REM python results\collect_erosion.py sa_lvl1_60
+@REM python results\collect_erosion.py sa_lvl1_61
+@REM python results\collect_erosion.py sa_lvl1_62
+@REM python results\collect_erosion.py sa_lvl1_63
+@REM python results\collect_erosion.py sa_lvl1_64
+@REM python results\collect_erosion.py sa_lvl1_65
+@REM python results\collect_erosion.py sa_lvl1_66
+@REM python results\collect_erosion.py sa_lvl1_67
+@REM python results\collect_erosion.py sa_lvl1_68
 
-python results\collect_erosion.py sa_lvl2_1
-python results\collect_erosion.py sa_lvl2_2
-python results\collect_erosion.py sa_lvl2_3
-python results\collect_erosion.py sa_lvl2_4
-python results\collect_erosion.py sa_lvl2_5
-python results\collect_erosion.py sa_lvl2_6
-python results\collect_erosion.py sa_lvl2_7
-python results\collect_erosion.py sa_lvl2_8
-python results\collect_erosion.py sa_lvl2_9
-python results\collect_erosion.py sa_lvl2_10
-python results\collect_erosion.py sa_lvl2_11
-python results\collect_erosion.py sa_lvl2_12
-python results\collect_erosion.py sa_lvl2_13
-python results\collect_erosion.py sa_lvl2_14
-python results\collect_erosion.py sa_lvl2_15
-python results\collect_erosion.py sa_lvl2_16
-python results\collect_erosion.py sa_lvl2_17
-python results\collect_erosion.py sa_lvl2_18
+@REM python results\collect_erosion.py sa_lvl2_1
+@REM python results\collect_erosion.py sa_lvl2_2
+@REM python results\collect_erosion.py sa_lvl2_3
+@REM python results\collect_erosion.py sa_lvl2_4
+@REM python results\collect_erosion.py sa_lvl2_5
+@REM python results\collect_erosion.py sa_lvl2_6
+@REM python results\collect_erosion.py sa_lvl2_7
+@REM python results\collect_erosion.py sa_lvl2_8
+@REM python results\collect_erosion.py sa_lvl2_9
+@REM python results\collect_erosion.py sa_lvl2_10
+@REM python results\collect_erosion.py sa_lvl2_11
+@REM python results\collect_erosion.py sa_lvl2_12
+@REM python results\collect_erosion.py sa_lvl2_13
+@REM python results\collect_erosion.py sa_lvl2_14
+@REM python results\collect_erosion.py sa_lvl2_15
+@REM python results\collect_erosion.py sa_lvl2_16
+@REM python results\collect_erosion.py sa_lvl2_17
+@REM python results\collect_erosion.py sa_lvl2_18
 
-python results\collect_erosion.py sa_lvl3_1
-python results\collect_erosion.py sa_lvl3_2
+@REM python results\collect_erosion.py sa_lvl3_1
+@REM python results\collect_erosion.py sa_lvl3_2
 
-python results\collect_erosion.py val_per1_no-therm
-python results\collect_erosion.py val_per2_no-therm
-python results\collect_erosion.py val_per3_no-therm
+@REM python results\collect_erosion.py val_per1_no-therm
+@REM python results\collect_erosion.py val_per2_no-therm
+@REM python results\collect_erosion.py val_per3_no-therm
 
-python results\collect_erosion.py val_per1_base
-python results\collect_erosion.py val_per2_base
-python results\collect_erosion.py val_per3_base
+@REM python results\collect_erosion.py val_per1_base
+@REM python results\collect_erosion.py val_per2_base
+@REM python results\collect_erosion.py val_per3_base
+
+python results\collect_erosion.py sa_base_10y
+python results\collect_erosion.py sa_base_10y
+python results\collect_erosion.py sa_base_10y
