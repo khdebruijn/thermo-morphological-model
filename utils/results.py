086a7562ce71d1ab6ad05c5c9137b67e6b775bae
Kevin <75491985+khdebruijn@users.noreply.github.com>
2025-01-20 16:31:35 +0100

    Update results.py

--- utils/results.py
+++ utils/results.py
@@ -663,7 +663,7 @@ class SimulationResults():
         
         # create initial plot for xbeach times plot
         axs[4].plot(np.arange(len(self.xbeach_times)), self.xbeach_times, color='k')
-        xticks = np.arange(0, 8760 + 730, 730)
+        xticks = np.arange(0, 8760, 730)
         xtick_labels = ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         axs[4].set_xticks(xticks)
         axs[4].set_xticklabels(xtick_labels, rot=10)
